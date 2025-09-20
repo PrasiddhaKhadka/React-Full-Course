00b21e03a93d6913d90742bd9063da2441e8a744
PrasiddhaKhadka <prasiddhak77@gmail.com>
2025-09-20 18:46:36 +0545

    Add: Lifting State

---
+++ src/LiftingState/LifitngStateSubComponent.jsx
@@ -0,0 +1,9 @@
+function LiftingStateSubComponent(){
+    return (
+        <div>
+            <h1>Lifting State Sub Component</h1>
+        </div>
+    )
+}
+
+export default LiftingStateSubComponent
---
+++ src/LiftingState/LiftingStateComponent.jsx
@@ -0,0 +1,9 @@
+function LiftingStateComponent(){
+    return (
+        <div>
+            <h1>Lifting State</h1>
+        </div>
+    )
+}
+
+export default LiftingStateComponent
